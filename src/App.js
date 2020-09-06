330d5bec4fed455600f10ff171cb118418e62fc8
bsrcs <bsr.ercelik@gmail.com>
2020-09-06 15:31:52 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import { Table } from "reactstrap"
+import axios from "axios"
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.log({ error }))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Countries</h3>
+      <Table hover size="sm">
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Capital</th>
+            <th>Region</th>
+            <th>Native Name</th>
+            <th>Flag</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((country) => (
+            <tr key={country.name}>
+              <td>{country.name}</td>
+              <td>{country.capital}</td>
+              <td>{country.region}</td>
+              <td>{country.nativeName}</td>
+              <td>
+                <img
+                  src={country.flag}
+                  alt={country.name}
+                  style={{ width: "100px" }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
